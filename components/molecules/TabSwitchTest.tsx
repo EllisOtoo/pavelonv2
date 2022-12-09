86cbc10eb74a77a1c7177711ed7d408e3d345cc7
Ellis Otoo <ellisotoo@gmail.com>
2022-12-09 12:00:15 +0000

    Changed to tabbed actions

--- components/molecules/TabSwitchTest.tsx
+++ components/molecules/TabSwitchTest.tsx
@@ -11,6 +11,7 @@ const TabSwitchTest = () => {
   useLayoutEffect(() => {
     setStartHeight(startingMenuItem.current.getBoundingClientRect().height);
   });
+
   useEffect(() => {
     const children = Array.from(menuParent.current.children);
     children.forEach((element) => {
@@ -29,7 +30,7 @@ const TabSwitchTest = () => {
     <div className="relative">
       <div
         ref={menuParent}
-        className="relative flex z-10 w-10/12 justify-between"
+        className="relative  px-4 flex z-10 py-4 px-2 w-10/12 justify-between"
       >
         {/** loop childelements and attach mouseover effect dynamically to all its children instead of hardCoding */}
         <div ref={startingMenuItem}>One</div>
@@ -47,12 +48,12 @@ const TabSwitchTest = () => {
           Two
         </div>
         <div>Two Hundred</div>
-        <div>Three Hundred</div>
+        <div>Hello</div>
       </div>
       <div
         ref={pillRef}
         style={{ height: `${startingMenuItemHeight}px` }}
-        className="absolute top-0 w-10 bg-yellow-300 rounded-xl"
+        className="absolute top-4 w-10  bg-yellow-300 rounded-xl"
       ></div>
     </div>
   );
