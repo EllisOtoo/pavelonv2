17bfffb084af09d9c37ff3a05aa9f2efc496bbd8
Ellis Otoo <ellisotoo@gmail.com>
2022-12-13 13:09:55 +0000

    UI alignment Issues

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link";
+
 const Button = ({ title }: { title: string }) => {
   return (
-    <button
-      onClick={() => {
-        console.log("Clicked");
-      }}
-      className="bg-gray-800 py-2 px-4 text-white w-8/12 md:w-3/12 rounded-lg hover:bg-sky-600
+    <Link href={"/contact"}>
+      {" "}
+      <button
+        onClick={() => {
+          console.log("Clicked");
+        }}
+        className="bg-gray-800 py-2 px-4 text-white w-8/12 md:w-6/12 rounded-lg hover:bg-sky-600
 "
-    >
-      {title}
-    </button>
+      >
+        {title}
+      </button>
+    </Link>
   );
 };
 
