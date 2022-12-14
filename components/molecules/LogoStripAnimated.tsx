3103b2142fd49d2e88299b502f625b0a92668e1d
Ellis Otoo <ellisotoo@gmail.com>
2022-12-14 11:20:15 +0000

    logo Area Updated

--- components/molecules/LogoStripAnimated.tsx
+++ components/molecules/LogoStripAnimated.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import SectionHeader from "../atoms/SectionHeader";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import { Paper } from "@mui/material";
+import { Paper, useMediaQuery } from "@mui/material";
 
 const styles = {
   root: {
     // padding: "0 30px", /* Show some extra logo on side */
-    padding: "0 0px",
+    padding: "0 30px",
   },
   slideContainer: {
     padding: "0 0px",
@@ -19,42 +19,67 @@ const styles = {
     color: "#fff",
   },
   slide1: {
-    background: "#FEA900",
+    // background: "#FEA900",
   },
   slide2: {
-    background: "#B3DC4A",
+    // background: "#B3DC4A",
   },
   slide3: {
-    background: "#6AC0FF",
+    // background: "#6AC0FF",
   },
   slide4: {
-    backgroundColor: "silver",
+    // backgroundColor: "silver",
   },
 };
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 function LogoStripAnimated({ imageUrls, headertitle }) {
+  const matches = useMediaQuery("(max-width:600px)");
+
   return (
     <div className="my-12 flex flex-col md:flex-row items-center justify-between w-10/12 m-auto">
       <p className="text-4xl w-12/12 md:w-6/12">
-        Powering businesses, small and large
+        Powering businesses, {matches ? <br /> : ""} small and large
       </p>
+
       <div className="w-full my-4 md:w-3/12">
         <AutoPlaySwipeableViews
           style={styles.root}
           slideStyle={styles.slideContainer}
         >
           <div style={Object.assign({}, styles.slide, styles.slide1)}>
-            Creditmall
+            <Image
+              width={200}
+              height={50}
+              alt=""
+              src={"/logostrip/credify.png"}
+            />
           </div>
           <div style={Object.assign({}, styles.slide, styles.slide2)}>
-            Credify
+            <Image
+              width={200}
+              height={50}
+              alt=""
+              src={"/logostrip/creditmall.png"}
+            />
           </div>
           <div style={Object.assign({}, styles.slide, styles.slide3)}>
-            Yango
+            <Image
+              width={200}
+              height={50}
+              alt=""
+              src={"/logostrip/yango.f54ab341.png"}
+            />
           </div>
-          <div style={Object.assign({}, styles.slide, styles.slide4)}>GBC</div>
+          <div style={Object.assign({}, styles.slide, styles.slide4)}>
+            <Image
+              width={200}
+              height={50}
+              alt=""
+              src={"/logostrip/gcb_logo.cb4c2a8d.png"}
+            />
+          </div>
         </AutoPlaySwipeableViews>
       </div>
     </div>
