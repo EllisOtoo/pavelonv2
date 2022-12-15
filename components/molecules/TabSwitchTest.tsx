6ca7b159069a97807895230db73ad6455544ae0a
Ellis Otoo <ellisotoo@gmail.com>
2022-12-15 13:52:58 +0000

    Investment as a service tabbed nav

--- components/molecules/TabSwitchTest.tsx
+++ components/molecules/TabSwitchTest.tsx
@@ -34,19 +34,7 @@ const TabSwitchTest = () => {
       >
         {/** loop childelements and attach mouseover effect dynamically to all its children instead of hardCoding */}
         <div ref={startingMenuItem}>One</div>
-        <div
-          /** Moved Event listener to useEffect, attached after component has mounted */
-          /* onMouseOver={(e) => {
-            anime({
-              targets: pillRef.current,
-              translateX: e.target.getBoundingClientRect().left - 10,
-              width: e.target.getBoundingClientRect().width + 20,
-            });
-          }} */
-          className=""
-        >
-          Two
-        </div>
+        <div>Two</div>
         <div>Two Hundred</div>
         <div>Hello</div>
       </div>
--- pages/investmentAsAService.tsx
+++ pages/investmentAsAService.tsx
@@ -8,6 +8,9 @@ import { Image, KeyboardArrowRightOutlined, Money } from "@mui/icons-material";
 import HeaderArea from "../components/organism/HeaderArea";
 import HeaderSubImage from "../components/organism/HeaderSubImage";
 import BaasBasicSection from "../components/organism/BaasBasicSection";
+import TabSwitchTest from "../components/molecules/TabSwitchTest";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import anime from "animejs";
 // import BAASSection  from "../components/organism/BoldSection";
 
 const BAAS_Section_Defaults = {
@@ -24,6 +27,7 @@ const BAAS_Section_Defaults = {
 const InvestmentAsAService = () => {
   return (
     <>
+      {/* <TabSwitchTest /> */}
       <HeaderSubImage
         reverse
         subHeader={"We support the full account lifecycle"}
@@ -38,8 +42,74 @@ const InvestmentAsAService = () => {
           scalable.
         </p>
       </HeaderSubImage>
+      <HeavyLiftingTabSwitch />
       <div></div>
     </>
+  );
+};
+
+const HeavyLiftingTabSwitch = () => {
+  const [startingMenuItemHeight, setStartHeight] = useState(0);
+  const [startingMenuItemWidth, setStartWidth] = useState(0);
+  const [startingMeuLeftX, setStartingMeuLeftX] = useState(0);
+  const [startingTopY, setstartingTopY] = useState(0);
+
+  const startingMenuItem = useRef(null);
+  const pillRef = useRef(null);
+  const menuParent = useRef(null);
+
+  useLayoutEffect(() => {
+    // window.getComputedStyle(startingMenuItem, null);
+    // console.log(startingMenuItem.current.getComputedStyle());
+    const startingMenuProps = window.getComputedStyle(startingMenuItem.current);
+
+    console.log(startingMenuProps.getPropertyValue("top"));
+
+    setStartHeight(startingMenuItem.current.getBoundingClientRect().height);
+    setStartWidth(startingMenuItem.current.getBoundingClientRect().width);
+    setStartingMeuLeftX(startingMenuItem.current.getBoundingClientRect().left);
+    setstartingTopY(startingMenuItem.current.getBoundingClientRect().top);
+  }, []);
+
+  useEffect(() => {
+    // console.log(
+    //   startingTopY,
+    //   startingMenuItem.current.getBoundingClientRect().top
+    // );
+    const children = Array.from(menuParent.current.children);
+    children.forEach((element) => {
+      element.addEventListener("mouseover", (e) => {
+        anime({
+          targets: pillRef.current,
+          translateX: e.target.getBoundingClientRect().left - 10,
+          width: e.target.getBoundingClientRect().width + 20,
+          height: e.target.getBoundingClientRect().height,
+        });
+      });
+    });
+  });
+
+  return (
+    <div className="relative">
+      <div
+        ref={menuParent}
+        className="relative px-4 flex z-10 py-4 px-2 w-10/12 justify-between m-auto"
+      >
+        {/** loop childelements and attach mouseover effect dynamically to all its children instead of hardCoding */}
+        <div ref={startingMenuItem}>Without Pavelon</div>
+        <div>With Pavelon</div>
+      </div>
+      <div
+        ref={pillRef}
+        style={{
+          height: `${startingMenuItemHeight}px`,
+          width: `${startingMenuItemWidth + 20}px`,
+          left: `${startingMeuLeftX}px`,
+          top: `${startingTopY}px`,
+        }}
+        className="absolute w-10  bg-yellow-300 rounded-xl"
+      ></div>
+    </div>
   );
 };
 
