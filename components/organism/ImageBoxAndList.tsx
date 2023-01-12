76f2001a96bf6cea56f5edda77727a284cd15c7d
Ellis Otoo <ellisotoo@gmail.com>
2023-01-12 10:50:43 +0000

    Pvaelon Api images

--- components/organism/ImageBoxAndList.tsx
+++ components/organism/ImageBoxAndList.tsx
@@ -9,9 +9,10 @@ function ImageBoxandList({ image, listItems, flexCol }) {
         flexCol && "flex-col"
       } md:flex-row w-10/12 m-auto gap-12 py-12 md:items-center md:h-screen `}
     >
-      <div className="relative h-44 w-12/12 md:w-6/12 md:h-96">
+      {/* <div className="relative h-44 w-12/12 md:w-6/12 md:max-h-96">
         <Image fill src={image} alt="" />
-      </div>
+      </div> */}
+      <Image width={500} height={400} src={image} alt="" />
 
       <div className={`${flexCol && "w-full"} md:w-6/12 `}>
         <ul className="grid grid-cols-1  divide-y divide-gray-200">
